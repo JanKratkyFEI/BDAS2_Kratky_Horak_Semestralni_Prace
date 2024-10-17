1569b00f485fa5dd461e433ea1e35c2800861eef
JanKratkyFEI <85405339+Honzajknize@users.noreply.github.com>
2024-10-17 08:46:43 +0200

    Add project files.

---
+++ BDAS2_Kratky_Horak_Semestralni_Prace/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using BDAS2_Kratky_Horak_Semestralni_Prace.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BDAS2_Kratky_Horak_Semestralni_Prace.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BDAS2_Kratky_Horak_Semestralni_Prace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BDAS2_Kratky_Horak_Semestralni_Prace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
